d6724f55714499bfd9f6e6a24c191432fb944313
Enrique Benitez <enriquebenitezjr@gmail.com>
2023-12-12 18:24:36 -0600

    feat: add link to nfts on tensor

--- src/app/[page]/page.tsx
+++ src/app/[page]/page.tsx
@@ -15,6 +15,7 @@ async function fetchStakeData(mints: number[]) {
 
     for (const stake of stakeData) {
       items.push({
+        id: mint,
         name: stake.json.name,
         image: stake.json.image,
         withdrawn: new BigNumber(stake.withdrawn).dividedBy(1000000000).toFixed(0).toLocaleString(),
@@ -77,21 +78,23 @@ export default async function Page({ params }: { params: { page: string } }) {
           <p>Made by <b><a href="https://x.com/0xPegasus" target="_blank" className="text-blue-500">@0xPegasus</a></b></p>
         </div>
       </div>
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left gap-2">
         {items.map((item, idx) => (
-          <div className="flex flex-col items-center justify-center p-4 space-y-4 bg-black rounded-lg shadow-lg" key={idx}>
-            <Image
-              src={item.image}
-              alt={item.name}
-              width={100}
-              height={100}
-              unoptimized
-            />
-            <div className="text-xl font-bold">{item.name}</div>
-            <div className="text-lg">Withdrawn: {item.withdrawn}</div>
-            <div className="text-lg">Harvested: {item.harvested}</div>
-            <div className="text-lg">Bonus Redeemed: {item.bonus_redeemed ? 'YES' : 'NO'}</div>
-          </div>
+          <a href={`https://www.tensor.trade/item/${item.id}`} target="_blank" key={idx}>
+            <div className="flex flex-col items-center justify-center p-4 space-y-4 bg-black rounded-lg shadow-lg border-slate-800 border hover:bg-fuchsia-500">
+              <Image
+                src={item.image}
+                alt={item.name}
+                width={100}
+                height={100}
+                unoptimized
+              />
+              <div className="text-xl font-bold text-center">{item.name}</div>
+              <div className="text-lg">Withdrawn: {item.withdrawn}</div>
+              <div className="text-lg">Harvested: {item.harvested}</div>
+              <div className="text-lg">Bonus Redeemed: {item.bonus_redeemed ? 'YES' : 'NO'}</div>
+            </div>
+          </a>
         ))}
       </div>
       <div className="flex justify-center items-center space-x-12 mt-16">
